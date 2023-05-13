528badf0e4c61c402cc736cc6fa5d8868257131b
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-05-13 18:03:00 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,9 @@
 # Resolve and fix 500 error from Apache web server
 
+$path_to_file_to_edit = '/var/www/html/wp-settings.php'
+
 # Replace phpp with php in /var/www/html/wp-settings.php
-exec {
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${path_to_file_to_edit}",
   path    => ['/bin','/usr/bin']
 }
